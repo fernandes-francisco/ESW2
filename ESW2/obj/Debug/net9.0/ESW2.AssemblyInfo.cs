df9bf626d49ac6feb28edd091378962db4d62de7
MiguelAzevedo57 <miguelazevedo@ipvc.pt>
2025-03-19 10:58:35 +0000

    login_registar

--- ESW2/obj/Debug/net9.0/ESW2.AssemblyInfo.cs
+++ ESW2/obj/Debug/net9.0/ESW2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ESW2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+885446afdab0c7cf5cc614cc3d431a8dd0fe0b31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed82ca177331eeef3687f9814618a523e88b011")]
 [assembly: System.Reflection.AssemblyProductAttribute("ESW2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ESW2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
