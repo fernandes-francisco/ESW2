134b3c17f3a31e563af46e6136ebc1e2dccd186b
simaoferreira1 <fsimao530@gmail.com>
2025-05-29 10:51:54 +0100

    realtorio do lucro total antes e depois dos impostos

---
+++ ESW2/Models/RelatorioLucroViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ESW2.Models
+{
+    public class RelatorioLucroViewModel
+    {
+        public DateTime? DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+        public List<LinhaRelatorioLucro> Linhas { get; set; } = new();
+
+        public decimal LucroTotalBruto => Linhas.Sum(l => l.LucroTotalBruto);
+        public decimal LucroTotalLiquido => Linhas.Sum(l => l.LucroTotalLiquido);
+        public decimal LucroMensalBruto => Linhas.Any() ? Linhas.Average(l => l.LucroMensalBruto) : 0;
+        public decimal LucroMensalLiquido => Linhas.Any() ? Linhas.Average(l => l.LucroMensalLiquido) : 0;
+    }
+
+    public class LinhaRelatorioLucro
+    {
+        public string NomeAtivo { get; set; } = string.Empty;
+        public decimal LucroTotalBruto { get; set; }
+        public decimal LucroTotalLiquido { get; set; }
+        public decimal LucroMensalBruto { get; set; }
+        public decimal LucroMensalLiquido { get; set; }
+    }
+}
