394909c24312dace5a9e8d250b82e33ac3c86032
simaoferreira1 <fsimao530@gmail.com>
2025-05-29 12:04:41 +0100

    .

---
+++ ESW2/Models/RelatorioImpostosViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ESW2.Models;
+public class RelatorioImpostosViewModel
+{
+    public DateTime DataInicio { get; set; }
+    public DateTime DataFim { get; set; }
+    public List<ImpostoMensalAtivo> Itens { get; set; } = new();
+}
+
+public class ImpostoMensalAtivo
+{
+    public string NomeAtivo { get; set; }
+    public int Ano { get; set; }
+    public int Mes { get; set; }
+    public decimal ValorTotalImposto { get; set; }
+}
