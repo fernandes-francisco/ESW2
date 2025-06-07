c6f1b4f03b261959d4f106a77ccebf62ac2fb458
RUFerreira33 <r.u.ferreira@ipvc.pt>
2025-06-07 16:50:27 +0100

    Página Apagar e relatorios melhorados

--- ESW2/obj/Debug/net9.0/ESW2.AssemblyInfo.cs
+++ ESW2/obj/Debug/net9.0/ESW2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ESW2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e06c6dcc16fa5c2e31fe44c0f64512af49b98b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195f8af8bcbcd99fc11ae2d3ce1801ed67b75e8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ESW2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ESW2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
